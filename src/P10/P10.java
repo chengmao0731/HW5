b53c07b227176a85f4d81f5de8c352f4339a7061
chengmao0731 <t110360125@ntut.org.tw>
2023-12-07 10:58:38 +0800

    HW5

---
+++ src/P10/P10.java
@@ -0,0 +1,9 @@
+package P10;
+
+public class P10 {
+	public static void main(String[] args)
+	{
+		P8.Car car1 = new P8.Car();
+		car1.show();
+	}
+}
---
+++ src/P8/P8.java
@@ -0,0 +1,10 @@
+package P8;
+
+public class P8 {
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+		car1.show();
+	}
+}
